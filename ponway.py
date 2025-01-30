f5be6417b666804efbfcac49b19a10e5d6855edd
EliotTarlov <eliottarlov@gmail.com>
2025-01-29 18:02:33 -0800

    way way pon pon pon...

--- ponway.py
+++ ponway.py
@@ -1,6 +1,1 @@
-import re
-goal="pon pon way way way pon pon way pon way pon pon, way way pon pon pon way way pon way pon way way"
-biney=[int(x=="pon") for x in goal.split()]
-strig=int("".join([str(x) for x in biney]),2)
-print(" ".join([int(x)*"pon" + (int(x)^1)*"way" \
-for x in bin(13018004)[2:]]))
+print(" ".join([["pon","way"][int(x)] for x in bin(13018004)[2:]]))
